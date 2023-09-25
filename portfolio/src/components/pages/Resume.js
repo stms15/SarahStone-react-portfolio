beb5cf05bea59237df924c85c49e8f6ee02b31e6
Sarah S <stms15@mun.ca>
2023-09-24 21:44:51 -0230

    added styling for Resume page

--- portfolio/src/components/pages/Resume.js
+++ portfolio/src/components/pages/Resume.js
@@ -1,34 +1,50 @@
 import React from 'react';
 import resume from '../../assets/files/SarahStone_Resume.pdf';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
 
 export default function Resume({ color }) {
   return (
     <div className="page" style={{ backgroundColor: color, color: 'white' }}>
-      <p>
-        Download my resume{' '}
-        <a href={resume} download="SarahStone_Resume">
-          here
-        </a>
-        .
-      </p>
+      <a href={resume} download="SarahStone_Resume" id="resumeLink">
+        <h3>
+          My Resume <FontAwesomeIcon icon={faDownload} />
+        </h3>
+      </a>
 
-      <h5>Front-end Strengths</h5>
-      <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JavaScript</li>
-        <li>React</li>
-        <li>Responsive Design</li>
-      </ul>
+      <div className="card-container">
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+          }}
+        >
+          <h5>Front-end Strengths</h5>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>React</li>
+            <li>Responsive Design</li>
+          </ul>
+        </div>
 
-      <h5>Back-end Strengths</h5>
-      <ul>
-        <li>Node.js</li>
-        <li>Express.js</li>
-        <li>APIs</li>
-        <li>MySQL, Sequelize</li>
-        <li>MongoDB, Mongoose</li>
-      </ul>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+          }}
+        >
+          <h5>Back-end Strengths</h5>
+          <ul>
+            <li>Node.js</li>
+            <li>Express.js</li>
+            <li>APIs</li>
+            <li>MySQL, Sequelize</li>
+            <li>MongoDB, Mongoose</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
